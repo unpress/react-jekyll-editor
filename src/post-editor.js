53bc738a5d1f6a3c8421b6d8b6d53401da6b5bd9
Niklas Närhinen <niklas@narhinen.net>
2016-03-06 02:04:38 +0200

    Add meta

--- src/post-editor.js
+++ src/post-editor.js
@@ -21,11 +21,26 @@ const PostEditor = React.createClass({
     return (
       <div className="je-post-editor">
         <div className="je-post-summary">
-          <h2>{title}</h2>
           <div className="je-editor-actions">
+            <h2>{title}</h2>
             <button className="je-save-button">Save</button>&nbsp;
             <button className="je-reset-button">Reset</button>&nbsp;
             <button className="je-delete-button">Delete</button>
+          </div>
+          <div className="je-post-meta">
+            <h4>Attributes</h4>
+            <div className="je-input-group">
+              <label htmlFor="je-fe-title">Title</label>
+              <div className="je-input-wrapper">
+                <input id="je-fe-title" type="text" value={title} />
+              </div>
+            </div>
+            <div className="je-input-group">
+              <label htmlFor="je-fe-date">Date</label>
+              <div className="je-input-wrapper">
+                <input id="je-fe-date" type="text" value={date} />
+              </div>
+            </div>
           </div>
         </div>
         <div className="je-post-post">
@@ -42,7 +57,7 @@ const PostEditor = React.createClass({
             value={this.state.body} />
         </div>
         <div className="je-post-preview">
-          <h2>Preview</h2>
+          <h2>{title}</h2>
           <div dangerouslySetInnerHTML={{__html: preview}} />
         </div>
       </div>
